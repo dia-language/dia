61d662fc3c02124ee19c7844ed635ddb05f628c4
Ch1keen <gihoong7@gmail.com>
2025-07-01 13:02:06 +0900

    Convert test to alcotest

--- dia.mli
+++ dia.mli
@@ -1,5 +1,7 @@
 val dia_dbgprint : string -> unit
 
+val dia_create_cpp_variable : int -> string
+
 val dia_debug_function_descriptor : DiaNode.dia_node -> int -> unit
 
 val dia_generate_code : DiaNode.dia_node -> DiaNode.dia_node list -> int -> DiaNode.dia_node * int
